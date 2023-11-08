f55ab48d04011eddd3942623c95e5cae13cc2438
GitHub Action <action@github.com>
2023-11-08 07:36:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/smango/solidoctojourney/MyBundle.kt
+++ src/main/kotlin/com/github/smango/solidoctojourney/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.smango.solidoctojourney
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/smango/solidoctojourney/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/smango/solidoctojourney/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.smango.solidoctojourney.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.smango.solidoctojourney.MyBundle
+import com.github.smango.solidoctojourney.services.MyProjectService
 import javax.swing.JButton
 
 
